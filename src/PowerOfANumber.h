2ec7b7977128695c3af69277300203ccd62f48b4
mariasher <mariasher001@gmail.com>
2025-03-21 00:22:49 +0100

    x^n---> using recursion algorithm Time Complexity = O(N). Causes stackoverflow for int_min

--- src/PowerOfANumber.h
+++ src/PowerOfANumber.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <ostream>
 
+//LeetCode 50: Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
 class PowerOfANumber {
 public:
     static double powerOfANumber(double x, int n) {
@@ -11,16 +13,26 @@ public:
         if (n == 0) {
            return 1;
         }
-
         if(n < 0) {
             result = powerOfANumber(x,n*-1);
             return 1/result;
-
         }
         for (int i = 0; i < n; ++i) {
             result *= x;
         }
         return result;
+    }
+
+    static double powerOfANumberWithRecursion(double x, int n) {
+        if(n==0) {
+            return 1;
+        }
+        if(n<0) {
+            n *= -1;
+            double R = x * powerOfANumberWithRecursion(x,n-1);
+            return 1/R;
+        }
+        return x * powerOfANumberWithRecursion(x,n-1);
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 int main() {
     const double R = PowerOfANumber::powerOfANumber(2.000, -2);
-    std::cout << "result is: " << R << std::endl;
+    const double R1 = PowerOfANumber::powerOfANumberWithRecursion(2.000, -2);
+
+    std::cout << "result is: " << R1 << std::endl;
     return 0;
 }
