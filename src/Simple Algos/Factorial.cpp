b1cc2ff10bcb11e8689cb7edab42e5e2b6b318d6
mariasher <mariasher001@gmail.com>
2025-06-03 22:36:34 +0200

    Calculating Factorial of a Number.

---
+++ src/Simple Algos/Factorial.cpp
@@ -0,0 +1,22 @@
+//
+// Created by msher on 6/3/2025.
+//
+
+#include "Factorial.h"
+
+int Factorial::n_Factorial(int n) {
+   int resultOfFactorial = 1;
+   for (int i = 1; i <= n; ++i) {
+      resultOfFactorial = resultOfFactorial * i;
+   }
+   return resultOfFactorial;
+
+}
+
+int Factorial::n_Factorial_Reverse(int n) {
+int resultOfFactorialReverse = n;
+   for (int i = n; i > 1; i--,n--) {
+      resultOfFactorialReverse = resultOfFactorialReverse*(n-1);
+   }
+   return resultOfFactorialReverse;
+}
