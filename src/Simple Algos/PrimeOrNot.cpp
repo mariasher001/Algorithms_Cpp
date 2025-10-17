df56926b551b002ab59e24671281479d5daa5f3b
mariasher <mariasher001@gmail.com>
2025-10-17 21:51:27 +0200

    Checking for Prime Numbers.

---
+++ src/Simple Algos/PrimeOrNot.cpp
@@ -0,0 +1,22 @@
+//
+// Created by msher on 10/17/2025.
+//
+
+#include "PrimeOrNot.h"
+
+#include <iostream>
+
+void PrimeOrNot::IsPrimeOrNot(int number) {
+	bool isPrime = false;
+	for (int i = 2; i < (number - 1) ; i++) {
+		if(number%i == 0) {
+			isPrime = true;
+			break;
+		}
+	}
+	if(isPrime) {
+		std::cout << number << " is not a prime number." << std::endl;
+	} else {
+		std::cout << number << " is a prime number." << std::endl;
+	}
+}
---
+++ src/Simple Algos/PrimeOrNot.h
@@ -0,0 +1,11 @@
+#ifndef PRIMEORNOT_H
+#define PRIMEORNOT_H
+
+class PrimeOrNot {
+public:
+	static void IsPrimeOrNot(int Number);
+};
+
+
+
+#endif //PRIMEORNOT_H
